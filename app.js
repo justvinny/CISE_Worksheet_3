6093ed0a1bfa6e1dcdbacea98bd41641b13dd2fb
Vinson Beduya <vinsonemb.1551994@gmail.com>
2022-03-21 20:09:59 +1300

    removed unused import

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const cors = require("cors");
-const path = require("path");
 
 // routes
 const books = require("./routes/api/books");
